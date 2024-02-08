002d51127a07f05ee28df91c24632f5e670c732a
Maciej Nikonowicz <nikon.maciej@gmail.com>
2024-02-08 18:44:52 +0100

    FullList class

--- src/model/ListItem.ts
+++ src/model/ListItem.ts
@@ -1,38 +1,38 @@
 export interface Item {
-    id:string,
-    item:string,
-    checked:boolean
+    id: string,
+    item: string,
+    checked: boolean
 }
 
 export default class ListItem implements Item {
 
     constructor (
-        private _id:string = '',
-        private _item:string = '',
-        private _checked:boolean = false
+        private _id: string = '',
+        private _item: string = '',
+        private _checked: boolean = false
     ) {}
 
-    get id():string {
+    get id(): string {
         return this._id;
     }
 
-    set id(id:string) {
+    set id(id: string) {
         this._id = id;
     }
 
-    get item():string {
+    get item(): string {
         return this._item;
     }
 
-    set item(item:string) {
+    set item(item: string) {
         this._item = item;
     }
 
-    get checked():boolean {
+    get checked(): boolean {
         return this._checked;
     }
 
-    set checked(checked:boolean) {
+    set checked(checked: boolean) {
         this._checked = checked;
     }
 }
